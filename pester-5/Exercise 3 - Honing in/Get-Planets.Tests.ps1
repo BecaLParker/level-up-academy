d010e371e3d4c4849fac5736726283c90898fea0
BecaLParker <becaparker@gmail.com>
2021-06-24 15:47:33 +0100

    Create a test fullfilling Planets have this order: Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune

--- pester-5/Exercise 3 - Honing in/Get-Planets.Tests.ps1
+++ pester-5/Exercise 3 - Honing in/Get-Planets.Tests.ps1
@@ -19,4 +19,16 @@ Describe "Get-Planets" {
     # TODO use the  `Where-Object` CmdLet.
     # $allPlanets | Where-Object { $_.Name -like "Earth" } | Should -BeEmptyOrNull
   }
+
+  It 'Planets have this order: Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune' {
+    $allPlanets[0].Name | Should -Be "Mercury"
+    $allPlanets[1].Name | Should -Be "Venus"
+    $allPlanets[2].Name | Should -Be "Earth"
+    $allPlanets[3].Name | Should -Be "Mars"
+    $allPlanets[4].Name | Should -Be "Jupiter"
+    $allPlanets[5].Name | Should -Be "Saturn"
+    $allPlanets[6].Name | Should -Be "Uranus"
+    $allPlanets[7].Name | Should -Be "Neptune"
+    #TODO: Refactor using -join operator
+  }
 }
